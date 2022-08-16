78ca4f5b6af1698fd73ef59ea1029fd2ecf3b596
thanh quach <thanhb.quach@gmail.com>
2022-08-16 17:49:59 -0400

    tile bug not fixed

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -8,16 +8,46 @@ const Wordle = () => {
   const [solution, setSolution] = useState('');
   const [guesses, setGuesses] = useState(Array(6).fill(null));
   const [currentGuess, setCurrentGuess] = useState('');
-  
+  const [isGameOver, setIsGameOver] = useState(false);
+
   useEffect(() => {
     const handleType = (event) => {
+
+        if (isGameOver) {
+            return;
+        }
+
+        if (event.key === 'Enter') {
+            if (currentGuess.length !== 5) {
+                return;
+            }
+            const newGuesses = [...guesses];
+            newGuesses[guesses.findIndex(val => val == null)] = currentGuess;
+            setGuesses(newGuesses);
+            setCurrentGuess('')
+
+            const isCorrect = solution === currentGuess;
+            if (isCorrect) {
+                setIsGameOver(true);
+            }
+        }
+
+        if(event.key === 'Backspace') {
+            setCurrentGuess(currentGuess.slice(0, -1));
+            return;
+        }
+
+        if (currentGuess.length >= 5) {
+            return;
+        }
+
         setCurrentGuess(oldGuess => oldGuess + event.key)
     };
 
     window.addEventListener('keydown', handleType);
 
     return () => window.addEventListener('keydown', handleType);
-  }, [])
+  }, [currentGuess, isGameOver, solution])
 
   useEffect(() => {
     fetchWord();
@@ -38,7 +68,11 @@ const Wordle = () => {
             guesses.map((guess, i) => {
                 const isCurrentGuess = i === guesses.findIndex(val => val == null);
                 return(
-                    <Line guess= { isCurrentGuess ? currentGuess: guess ?? '' } /> // a guess word or an empty string
+                    <Line 
+                    guess={isCurrentGuess ? currentGuess : guess ?? ''}
+                    isFinal={!isCurrentGuess && guess != null}
+                    solution={solution}
+                    /> // a guess word or an empty string
                 )
             })
         }
@@ -46,12 +80,24 @@ const Wordle = () => {
   )
 }
 
-const Line = ({guess}) => {
+const Line = ({guess, isFinal, solution}) => {
     const tiles = []
 
     for(let i = 0; i < WORD_LENGTH; i++){
         const char = guess[i] // here we have the edgge case: what if we don't have any words? - fixing line 26
-        tiles.push(<div key={i} className='tile'>{char}</div>)
+        let className = 'tile';
+        if(isFinal) {
+            if(char === solution[i]){
+                className += 'correct';
+            } else if (solution.includes(char)) {
+                className += 'close';
+            } else {
+                className += 'incorrect';
+            }
+        }
+
+
+        tiles.push(<div key={i} className={className}>{char}</div>)
     }
     return (
         <div className='line'>
---
+++ src/components/testing.js
@@ -0,0 +1,6 @@
+let i = 8
+let str = '8'
+
+const isCurrentGuess = i == str
+
+console.log(isCurrentGuess)
